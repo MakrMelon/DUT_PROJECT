638f99f943def06af001bc83cf8ab15b428228ef
Mark <mark@MarkdeMacBook-Pro.local>
2017-01-18 18:06:24 +0800

    使用wtforms

--- hello.py
+++ hello.py
@@ -1,14 +1,23 @@
 # _*_ coding:utf-8 _*_
-from flask import Flask,render_template
+
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask import request
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 #时间相关
 from datetime import datetime
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'blog for myself string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(Form):
+	name = StringField("What's your name?",validators=[Required()])
+	submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -18,12 +27,16 @@ def page_not_found(e):
 def internal_server_error(e):
 	return render_template('500.html'),500
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-	'''user_agent = request.headers.get('User-Agent')
-	print request.headers
-	return '<p>Your browser is %s</p>'%user_agent'''
-	return render_template('index.html',current_time=datetime.utcnow())
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html',current_time=datetime.utcnow(),name=session.get('name'),form=form)
 
 @app.route('/user/<name>')
 def user(name):
