133f87a73e9c1689ae9a743d8613beb76a6b6e9c
Mark <mark@MarkdeMacBook-Pro.local>
2017-01-20 17:40:16 +0800

    添加mail模块

--- hello.py
+++ hello.py
@@ -42,6 +42,7 @@ class User(db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer,primary_key=True)
 	username = db.Column(db.String(64),unique=True,index=True)
+	#age = db.Column(db.Integer,unique=False,default=10)
 	role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
 	def __repr__(self):
