dbb688502444f9fa89d07c139322d84d5f161f73
Mark <mark@MarkdeMacBook-Pro.local>
2017-02-06 16:09:51 +0800

    配置文件和初始化文件

---
+++ blog/__init__.py
@@ -0,0 +1,24 @@
+# _*_ coding:utf-8 _*_
+from flask import Flask
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.mail import Mail 
+from config import config
+
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+mail = Mail()
+
+def create_app(config_type):
+	app = Flask(__name__)
+	app.config.from_object(config[config_type])
+	config[config_type].init_app(app)
+	bootstrap.init_app(app)
+	moment.init_app(app)
+	db.init_app(app)
+	mail.init_app(app)
+
+	return app
---
+++ config.py
@@ -0,0 +1,43 @@
+# _*_ coding:utf-8 _*_
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+
+	SECRET_KEY = 'blog for myself string'   #表单安全
+	SQLALCHEMY_COMMIT_ON_TEARDOWN = True #自动提交数据库中的变动
+	#验证邮箱配置
+	MAIL_SERVER = 'smtp.qq.com'
+	MAIL_PREFIX = '欢迎来到本博客'
+	ADMIN = '879671510@qq.com'
+
+	@staticmethod
+	def init_app(app):
+		pass
+
+
+class DevelopConfig(Config):
+	DEBUG = True
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True  #启用传输层安全|
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or '879671510@qq.com'
+	MAIL_PASSWORD = os.environ.get('MAIL_CODE') or 'ipnokqkszfxubfgb'
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:211314@localhost:3306/blog'
+
+
+class TestConfig(Config):
+	TESTING = True
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:211314@localhost:3306/blog'
+	
+
+config = {
+	'development': DevelopConfig,
+	'test': TestConfig,
+	'production': ProductionConfig,
+	'default': DevelopConfig
+}
