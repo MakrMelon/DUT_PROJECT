f2c08035af5afaafb98473fadb385ddb0270ed33
Mark <mark@MarkdeMacBook-Pro.local>
2017-02-09 14:20:20 +0800

    添加unconfirmed页面  以及重新发送邮件提示

--- blog/auth/views.py
+++ blog/auth/views.py
@@ -40,6 +40,16 @@ def confirm(token):
 	return redirect(url_for('main.index'))
 
 
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_token()
+    send_email(current_user.email,'Confirm Your Account',
+                    'auth/email/confirm',user=current_user,token=token)
+    flash('A confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
+
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -61,4 +71,27 @@ def logout():
 	logout_user()
 	flash('You have logged out.')
 	return redirect(url_for('main.index'))
+
+@auth.before_app_request
+def before_request():
+	'''
+	(1) 用户已登录(current_user.is_authenticated() 必须返回 True)。
+	(2) 用户的账户还未确认。
+	(3) 请求的端点(使用 request.endpoint 获取)不在认证蓝本中。访问认证路由要获取权
+ 		限,因为这些路由的作用是让用户确认账户或执行其他账户管理操作。
+	'''
+	if current_user.is_authenticated\
+		and not current_user.confirmed\
+		and request.endpoint[:5] != 'auth.'\
+		and request.endpoint != 'static':
+		return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+	if current_user.is_anonymous or current_user.confirmed:
+		return redirect(url_for('main.index'))
+	return render_template('auth/unconfirmed.html')
+
+
 
