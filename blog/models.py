bdcf13e9b95a38b680582b3b5f1fc5b3cceda02a
Mark <mark@MarkdeMacBook-Pro.local>
2017-02-16 13:59:36 +0800

    添加Follow表和关注函数  外键

--- blog/models.py
+++ blog/models.py
@@ -62,6 +62,13 @@ class Role(db.Model):
 
 
 
+class Follow(db.Model):
+	__tablename__ = 'follows'
+	follower_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+	followed_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+	timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 class User(UserMixin,db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer,primary_key=True)
@@ -79,6 +86,41 @@ class User(UserMixin,db.Model):
 	last_seen = db.Column(db.DateTime(),default=datetime.utcnow)
 	avatar_hash = db.Column(db.String(32))  
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+	#关注相关
+	'''
+		某个用户关注了 100 个用户,调用 user.followed.all() 后会返回一个列 表,其中包含 100 个 Follow 实例,每一
+	个实例的 follower 和 followed 回引属性都指向相 应的用户。设定为 lazy='joined' 模式,就可在一次数据库查询中
+	完成这些操作。如果把 lazy 设为默认值 select,那么首次访问 follower 和 followed 属性时才会加载对应的用户, 而
+	且每个属性都需要一个单独的查询,这就意味着获取全部被关注用户时需要增加 100 次 额外的数据库查询。这两个关系中,User
+	一侧设定的 lazy 参数作用不一样。lazy 参数都在“一”这一侧设定, 返回的结果是“多”这一侧中的记录。
+		cascade 参数配置在父对象上执行的操作对相关对象的影响。比如,层叠选项可设定为: 将用户添加到数据库会话后,要自
+	动把所有关系的对象都添加到会话中。	层叠选项的默认 值能满足大多数情况的需求,但对这个多对多关系来说却不合用。删除
+	对象时,默认的层 叠行为是把对象联接的所有相关对象的外键设为空值。但在关联表中,删除记录后正确的行为应该是把指向该记录
+	的实体也删除,因为这样能有效销毁联接。这就是层叠选项值 delete-orphan 的作用。cascade 参数的值是一组由逗号分隔的
+	层叠选项,这看起来可能让人有 点困惑,但 all 表示除了 delete-orphan 之外的所有层叠选项。设为 all, delete-orphan 
+	的意思是启用所有默认层叠选项,而且还要删除孤儿记录。
+
+	'''
+	#followed 和 followers 关系都定义为单独的一对多关系。
+	followed = db.relationship('Follow',
+								#为了消除外键间的歧义,定义关系时必须使用可选参数 foreign_keys 指定的外键
+								foreign_keys=[Follow.follower_id],
+								#并不是指定这两个关系之间的引用关系,而是回引Follow 模型。回引中的 lazy 
+								#参数指定为 joined。这个 lazy 模式可以实现立即从联结查询中加载相关对象
+								backref=db.backref('follower', lazy='joined'),
+								#关系属性不会直 接返回记录,而是返回查询对象,所以在执行查询之前还可以添加额外的过滤器。
+								lazy='dynamic',
+								cascade='all, delete-orphan')
+	followers = db.relationship('Follow',
+								foreign_keys=[Follow.followed_id],
+								backref=db.backref('followed', lazy='joined'),
+								lazy='dynamic',
+								cascade='all, delete-orphan')
+
+
+
+
 
 	def __init__(self,**kwargs):
 		super(User,self).__init__(**kwargs)
@@ -161,6 +203,27 @@ class User(UserMixin,db.Model):
 			except IntegrityError:
 				db.session.rollback()
 
+	#关注辅助方法
+	def is_following(self,user):
+		#关注者是self，被关注的人是user
+		return self.followed.filter_by(followed_id=user.id).first() is not None
+
+	def is_followed_by(self,user):
+		#被关注者是self,关注者是user
+		return self.followers.filter_by(follower_id=user.id).first() is not None
+
+	#关注
+	def follow(self,user):
+		if not self.is_following(user):
+			f = Follow(follower=self, followed=user)
+			db.session.add(f)
+
+	#取消关注
+	def unfollow(self,user):
+		f = self.followed.filter_by(followed_id=user.id).first()
+		if f:
+			db.session.delete(f)
+
 
 class Post(db.Model):
 	__tablename__ = 'posts'
@@ -204,6 +267,8 @@ class Post(db.Model):
 														tags=allowed_tags, strip=True))
 		
 db.event.listen(Post.body, 'set', Post.on_changed_body)
+
+	
 
 
 '''
